c254aa65f118a2253c2f98ce40e0d392055bcb44
Aaditya Raj Karki <karkiaaditya2@gmail.com>
2026-06-16 03:23:49 -0400

    api gateway setup for eureka server

--- api-gateway/src/main/java/com/scotiabank/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/scotiabank/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,20 @@ package com.scotiabank.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 
 }
